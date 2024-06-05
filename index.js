535f4d9ae69e81fd7d53733a701f2887b4467321
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-06-05 08:24:32 +0700

    Cấu hình Database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require('dotenv').config();
 
+const database = require("./config/database");
+database.connect();
+
 const routeClient = require("./routes/client/index.route")
 
 const app = express();

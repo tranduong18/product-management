ce6b9f642680d0280c2ec2fa9d7ba3d36ee567c5
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-06-05 08:52:06 +0700

    Làm trang danh sách sản phẩn bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,21 +1,19 @@
+const Product = require("../../models/product.model");
+
 // [GET] /products/
-module.exports.index = (req, res) => {
+module.exports.index = async(req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    for(const item of products){
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+    console.log(products);
+
     res.render("client/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm"
+        pageTitle: "Danh sách sản phẩm",
+        products: products
     });
 }
-
-// [POST] /products/create
-// module.exports.create = (req, res) => {
-//     res.render("client/pages/products/create");
-// }
-
-// [PATCH] /products/edit
-// module.exports.edit = (req, res) => {
-//     res.render("client/pages/products/edit");
-// }
-
-// [GET] /products/detail
-// module.exports.detail = (req, res) => {
-//     res.render("client/pages/products/detail");
-// }
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -4,12 +4,6 @@ const router = express.Router();
 const controller = require("../../controllers/client/product.controller");
 
 
-router.get("/", controller.index);  
-
-// router.post("/create", controller.create);  
-
-// router.patch("/edit", controller.edit);  
-
-// router.get("/detail", controller.detail);  
+router.get("/", controller.index);    
 
 module.exports = router;

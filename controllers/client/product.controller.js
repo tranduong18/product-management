202daf279c9694b5be7848e302ea4050b0d03231
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-06-05 07:45:33 +0700

    Cấu trúc lại folder PUG

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 // [GET] /products/
 module.exports.index = (req, res) => {
-    res.render("client/pages/products/index");
+    res.render("client/pages/products/index", {
+        pageTitle: "Danh sách sản phẩm"
+    });
 }
 
 // [POST] /products/create

61d1702b7486af64bff67d78d5bb180b2632873d
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-11 17:21:04 +0700

    1.3. Làm giao diện danh sách sản phẩm

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,7 @@
 const dashboardRoute = require("./dashboard.route");
+const productsRoute = require("./product.route");
 
 module.exports.index = (app) => {
     app.use("/admin/dashboard", dashboardRoute);
+    app.use("/admin/products", productsRoute);
 }

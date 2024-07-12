eaa5ca4f0668cece956dd7677269d1604e920662
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-12 10:41:40 +0700

    4. Tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -10,12 +10,22 @@ module.exports.index = async(req, res) => {
         find.status = req.query.status;
     }
 
+    // Tìm kiếm
+    let keyword = "";
+    if(req.query.keyword){
+        const regex = new RegExp(req.query.keyword, "i");
+        find.title = regex;
+        keyword = req.query.keyword;
+    }
+    // Hết Tìm kiếm
+
     const products = await Product.find(find);
 
     // console.log(products);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Quản lý sản phẩm",
-        products: products
+        products: products,
+        keyword: keyword
     });
 }

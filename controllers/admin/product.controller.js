c887dddb10490c67ace741ec90484f864e39766e
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-12 08:40:18 +0700

    3. Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,9 +2,15 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products/
 module.exports.index = async(req, res) => {
-    const products = await Product.find({
+    const find = {
         deleted: false
-    });
+    };
+
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
 
     // console.log(products);
 

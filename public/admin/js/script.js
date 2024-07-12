fc7be35b3afb5205b40313f3e645367eac38daa7
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-12 09:31:51 +0700

    3. Tính năng lọc theo trạng thái

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,27 @@
-console.log("OK");
+// Button Status
+const listButtonStatus = document.querySelectorAll("[button-status]");
+if (listButtonStatus.length > 0) {
+    let url = new URL(window.location.href);
+
+    // Bắt sự kiện click
+    listButtonStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+
+            if (status) {
+                url.searchParams.set("status", status);
+            } else {
+                url.searchParams.delete("status");
+            }
+            window.location.href = url.href;
+        });
+    });
+
+    // Thêm class active mặc định
+    const statusCurrent = url.searchParams.get("status") || "";
+    const buttonCurrent = document.querySelector(`[button-status="${statusCurrent}"]`);
+    if(buttonCurrent){
+        buttonCurrent.classList.add("active");
+    }
+}
+// End Button Status

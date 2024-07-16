9b26e51a6e72ddd87d181bf9fc5b7781210d1a08
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-16 17:08:00 +0700

    4. Tính năng tạo mới sản phẩm - Thêm trường createdAt, updatedAt và slug tự động sinh ra

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
 const productSchema = new mongoose.Schema({
     title: String,
@@ -13,6 +15,13 @@ const productSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    slug:{
+        type: String,
+        slug: "title",
+        unique: true
+    }
+}, {
+    timestamps: true
 });
 
 const Product = mongoose.model("Product", productSchema, "products");

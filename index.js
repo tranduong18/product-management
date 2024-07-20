e55201273e9d4dead855a541adaf3b0f05846c32
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-20 14:59:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //parse application/json
 app.use(bodyParser.json());
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
 
 // App Locals Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;

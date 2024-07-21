7d5bb6a3c04a52df782cbc06643dd862abdc134e
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-21 15:35:08 +0700

    1. Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -44,4 +44,48 @@ module.exports.createPost = async (req, res) => {
     await newCategory.save();
   
     res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
-  }
+}
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+    const id = req.params.id;
+
+    const category = await ProductCategory.findOne({
+        _id: id,
+        deleted: false
+    });
+
+    const categories = await ProductCategory.find({
+        deleted: false
+    });
+
+    const newCategories = createTreeHelper(categories);
+
+    res.render("admin/pages/products-category/edit", {
+        pageTitle: "Chỉnh sửa danh mục sản phẩm",
+        categories: newCategories,
+        category: category
+    });
+}
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    if(req.body.position) {
+      req.body.position = parseInt(req.body.position);
+    } else {
+      const countCagegory = await ProductCategory.countDocuments({});
+      req.body.position = countCagegory + 1;
+    }
+
+    await ProductCategory.updateOne({
+        _id: id,
+        deleted: false
+    }, req.body);
+    
+    req.flash("success", "Cập nhật danh mục thành công");
+    
+    res.redirect("back");
+}
+

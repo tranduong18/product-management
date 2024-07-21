0c782488943361a3b0bef07438b1f6d1d18569fd
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-21 14:56:43 +0700

    3. Xây dựng phần quản lý danh mục sản phẩm (Phân cấp cha con cho danh mục)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,6 +1,8 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/createTree.helper");
+
 // [GET] /admin/products-category/
 module.exports.index = async (req, res) => {
     const records = await ProductCategory.find({
@@ -21,9 +23,11 @@ module.exports.create = async (req, res) => {
         deleted: false
     });
 
+    const newCategories = createTreeHelper(categories);
+
     res.render("admin/pages/products-category/create", {
         pageTitle: "Thêm mới danh mục sản phẩm",
-        categories: categories
+        categories: newCategories
     });
 }
 

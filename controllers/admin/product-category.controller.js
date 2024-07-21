2ffbeb0574e69e0e39bd10e88fcbf10bbcfd5fcf
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-21 11:16:00 +0700

    3. Xây dựng phần quản lý danh mục sản phẩm (Chọn danh mục cha dạng dropdown)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -17,8 +17,13 @@ module.exports.index = async (req, res) => {
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+    const categories = await ProductCategory.find({
+        deleted: false
+    });
+
     res.render("admin/pages/products-category/create", {
-        pageTitle: "Thêm mới danh mục sản phẩm"
+        pageTitle: "Thêm mới danh mục sản phẩm",
+        categories: categories
     });
 }
 

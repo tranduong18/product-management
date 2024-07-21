4e959b2be4ee3f1a40afb1bf057509caf55a4eaf
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-21 10:57:50 +0700

    3. Xây dựng phần quản lý danh mục sản phẩm (Thêm mới danh mục)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,6 +1,31 @@
+const ProductCategory = require("../../models/product-category.model");
+const systemConfig = require("../../config/system");
+
 // [GET] /admin/products-category/
 module.exports.index = (req, res) => {
     res.render("admin/pages/products-category/index", {
         pageTitle: "Danh mục sản phẩm"
     });
 }
+
+// [GET] /admin/products-category/create
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/products-category/create", {
+        pageTitle: "Thêm mới danh mục sản phẩm"
+    });
+}
+
+// [POST] /admin/products-category/create
+module.exports.createPost = async (req, res) => {
+    if(req.body.position) {
+      req.body.position = parseInt(req.body.position);
+    } else {
+      const countCagegory = await ProductCategory.countDocuments({});
+      req.body.position = countCagegory + 1;
+    }
+  
+    const newCategory = new ProductCategory(req.body);
+    await newCategory.save();
+  
+    res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+  }
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const multer = require('multer');
 const router = express.Router();
 
 const controller = require("../../controllers/admin/product-category.controller");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+const upload = multer();
 
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+    "/create", 
+    upload.single('thumbnail'),
+    uploadCloud.uploadSingle,
+    controller.createPost
+);
 
 module.exports = router;

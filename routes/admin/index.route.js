55a99d886cb1429ee11aa7f5922532b29ade9db5
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-23 14:18:11 +0700

    3.2. Xây dựng phần Danh sách nhóm quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 const dashboardRoute = require("./dashboard.route");
 const productsRoute = require("./product.route");
 const productsCategoryRoute = require("./product-category.route");
+const rolesRoute = require("./role.route");
 const systemConfig = require("../../config/system");
 
 module.exports.index = (app) => {
@@ -11,4 +12,6 @@ module.exports.index = (app) => {
     app.use(`${path}/products`, productsRoute);
 
     app.use(`${path}/products-category`, productsCategoryRoute);
+
+    app.use(`${path}/roles`, rolesRoute);
 }

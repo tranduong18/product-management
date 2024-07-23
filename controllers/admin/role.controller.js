83af5c6600e30e227b3a51ef51506fc98f1c9416
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-23 14:41:42 +0700

    3.4. Xây dựng phần Chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -15,7 +15,7 @@ module.exports.index = async (req, res) => {
 // [GET] /admin/roles/create
 module.exports.create = async (req, res) => {
     res.render("admin/pages/roles/create", {
-        pageTitle: "Tạo mới nhóm quyền",
+        pageTitle: "Thêm mới nhóm quyền",
     });
 }
 
@@ -25,4 +25,41 @@ module.exports.createPost = async (req, res) => {
     await record.save();
 
     res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const record = await Role.findOne({
+            _id: id,
+            deleted: false
+        });
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Chỉnh sửa nhóm quyền",
+            record: record
+        });
+    } catch (error) {
+        res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+    }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = req.body;
+
+        await Role.updateOne({
+            _id: id,
+            deleted: false
+        }, data);
+
+        req.flash("success", "Cập nhật thành công!");
+        res.redirect("back");
+    } catch (error) {
+        req.flash("error", "Cập nhật thất bại!");
+        res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+    }
 }

7cddd48d0d67e1d1bd10c810e3fd02887d922650
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-23 14:28:13 +0700

    3.3. Xây dựng phần Thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,5 @@
 const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
 // [GET] /admin/roles/
 module.exports.index = async (req, res) => {
@@ -9,4 +10,19 @@ module.exports.index = async (req, res) => {
         pageTitle: "Nhóm quyền",
         records: records
     });
+}
+
+// [GET] /admin/roles/create
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/roles/create", {
+        pageTitle: "Tạo mới nhóm quyền",
+    });
+}
+
+// [POST] /admin/roles/create
+module.exports.createPost = async (req, res) => {
+    const record = new Role(req.body);
+    await record.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }

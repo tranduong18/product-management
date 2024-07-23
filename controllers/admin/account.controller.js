a3be267a7d4fc20192246d7929dbd92c425c6527
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-23 20:01:29 +0700

    1.3. Làm trang tạo tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,6 +1,38 @@
+const Role = require("../../models/role.model");
+const Account = require("../../models/account.model");
+const md5 = require('md5');
+
+const generateHelper = require("../../helpers/generate.helper");
+const systemConfig = require("../../config/system");
+
 // [GET] /admin/accounts
 module.exports.index = (req, res) => {
     res.render("admin/pages/accounts/index", {
         pageTitle: "Tài khoản admin"
     });
 }
+
+// [GET] /admin/accounts/create
+module.exports.create = async(req, res) => {
+    const roles = await Role.find({
+        deleted: false
+    }).select("title");
+
+    res.render("admin/pages/accounts/create", {
+        pageTitle: "Tạo tài khoản admin",
+        roles: roles
+    });
+}
+
+// [POST] /admin/accounts/create
+module.exports.createPost = async(req, res) => {
+    req.body.password = md5(req.body.password);
+
+    req.body.token = generateHelper.generateRandomString(30);
+    
+    const account = new Account(req.body);
+    await account.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
+}
+
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const multer  = require('multer');
 const router = express.Router();
 
 const controller = require("../../controllers/admin/account.controller");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+const upload = multer();
 
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+    "/create",
+    upload.single('avatar'),
+    uploadCloud.uploadSingle,
+    controller.createPost
+);
 
 module.exports = router;

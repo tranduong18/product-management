534dfa8f26f7d9ed228987cf85ec7ebe531d51b0
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-24 17:51:02 +0700

    3. Làm tính năng đăng xuất

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -40,3 +40,9 @@ module.exports.loginPost = async (req, res) => {
     res.cookie("token", account.token);
     res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
 }
+
+// [GET] /admin/auth/logout
+module.exports.logout = async (req, res) => {
+    res.clearCookie("token");
+    res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+}

84f01c424e7125e971bc775f5ff90c5555488211
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-25 18:06:48 +0700

    2. Hiển thị menu chung bên client

--- controllers/admin/profile.controller.js
+++ controllers/admin/profile.controller.js
@@ -1,6 +1,5 @@
 // [GET] /admin/profile/
 module.exports.index = (req, res) => {
-    console.log(res.locals.account);
     res.render("admin/pages/profile/index", {
         pageTitle: "Thông tin cá nhân"
     });

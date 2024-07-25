7867b748e62ed3aeca8b81a1fa52d5ba559b2247
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-25 15:16:29 +0700

    1. Thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 module.exports.requireAuth = async (req, res, next) => {
     if(!req.cookies.token){
@@ -10,12 +11,19 @@ module.exports.requireAuth = async (req, res, next) => {
     const account = await Account.findOne({
         token: req.cookies.token,
         deleted: false
-    });
+    }).select("fullName email phone avatar role_id");
 
     if(!account){
         res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
         return;
     }
 
+    const role = await Role.findOne({
+        _id: account.role_id,
+    }).select("title permissions");
+    
+    res.locals.account = account;
+    res.locals.role = role;
+    
     next();
 }

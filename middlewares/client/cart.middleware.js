3da611f039842f9ec6203ce7b25e394f4012d02b
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-26 20:56:28 +0700

    2.2. Làm nút Mini Cart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -11,6 +11,13 @@ module.exports.cartId = async (req, res, next) => {
             expires: new Date(Date.now() + expires)
         });
     }
+    else{
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
+
+        res.locals.cartTotal = cart.products.length || 0;
+    }
     
     next();
 }

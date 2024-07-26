b7643d8dcc8cb89d2206a41cf21a83895cd3c8d0
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-26 15:14:05 +0700

    3. Thêm phần sản phẩm nổi bật bên admin

--- models/product.model.js
+++ models/product.model.js
@@ -10,6 +10,7 @@ const productSchema = new mongoose.Schema({
     discountPercentage: Number,
     stock: Number,
     thumbnail: String,
+    featured: String,
     status: String,
     position: Number,
     createdBy: String,

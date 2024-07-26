49f50f41a0487f71381d094048b6f74da4feb44a
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-26 15:24:00 +0700

    4.1 và 4.2

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,35 @@
+const Product = require("../../models/product.model");
+
 // [GET] /
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const productsFeatured = await Product.find({
+        featured: "1",
+        status: "active",
+        deleted: false
+    })
+    .sort({ position : "desc" }).limit(6)
+    .select("-description");
+
+    for(const item of productsFeatured){
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+
+    const productsNew = await Product.find({
+        status: "active",
+        deleted: false
+    })
+    .sort({ position : "desc" }).limit(6)
+    .select("-description");
+
+    for(const item of productsNew){
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+
+
+
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        productsFeatured: productsFeatured,
+        productsNew: productsNew
     });
 }

0535bfdc2d3b1a39616d4048e391857a0ea0f965
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-26 20:51:18 +0700

    2.1. Thêm sản phẩm vào giỏ hàng

---
+++ controllers/client/cart.controller.js
@@ -0,0 +1,40 @@
+const Cart = require("../../models/cart.model");
+
+// [POST] /cart/add/:productId
+module.exports.addPost = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const productId = req.params.productId;
+    const quantity = parseInt(req.body.quantity);
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+
+    const existProductInCart = cart.products.find(
+        item => item.productId == productId
+    );
+    
+    if(existProductInCart){
+        await Cart.updateOne({
+            _id: cartId,
+            'products.productId': productId
+        }, {
+            $set: {
+                'products.$.quantity': quantity + existProductInCart.quantity
+            }
+        });
+    }
+    else{
+        await Cart.updateOne({
+            _id: cartId
+        }, {
+            $push: {
+                products: {
+                    productId: productId,
+                    quantity: quantity
+                }
+            }
+        });
+    }
+    res.redirect("back");
+}

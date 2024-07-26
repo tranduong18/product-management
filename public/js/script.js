12eb7f5c050365a294e18c37a87f296440f119f5
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-26 21:59:10 +0700

    2.5. Cập nhật số lượng sản phẩm trong giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
-console.log("OK");
+// Cập nhật số lượng sản phẩm trong giỏ hàng
+const listInputQuantity = document.querySelectorAll("[cart] input[name='quantity']");
+if(listInputQuantity){
+    listInputQuantity.forEach(input => {
+        input.addEventListener("change", () => {
+            const productId = input.getAttribute("product-id");
+            const quantity = parseInt(input.value);
+            
+            if(productId && quantity > 0){
+                window.location.href = `/cart/update/${productId}/${quantity}`;
+            }
+        });
+    });
+}
+
+
+
+// Hết Cập nhật số lượng sản phẩm trong giỏ hàng

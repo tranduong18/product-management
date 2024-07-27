6ae914ac5747d1ff2125e45f1e5d251e569666d6
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-27 08:00:44 +0700

    1.3. Lấy data được gửi lên lưu vào database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,5 +1,6 @@
 const Cart = require("../../models/cart.model");
 const Product = require("../../models/product.model");
+const Order = require("../../models/order.model");
 
 // [GET] /checkout/
 module.exports.index = async (req, res) => {
@@ -27,4 +28,49 @@ module.exports.index = async (req, res) => {
         pageTitle: "Đặt hàng",
         cartDetail: cart
       });
+}
+
+// [GET] /checkout/
+module.exports.orderPost = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const userInfo = req.body;
+
+    const orderData = {
+        userInfo: userInfo,
+        products: []
+    };
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+
+    for(const item of cart.products){
+        const productInfo = await Product.findOne({
+            _id: item.productId
+        });
+
+        orderData.products.push({
+            productId: item.productId,
+            price: productInfo.price,
+            discountPercentage: productInfo.discountPercentage,
+            quantity: item.quantity
+        });
+
+        await Product.updateOne({
+            _id: item.productId
+        }, {
+            stock: productInfo.stock - item.quantity
+        });
+    }
+
+    const order = new Order(orderData);
+    await order.save();
+
+    await Cart.updateOne({
+        _id: cartId
+    }, {
+        products: []
+    });
+
+    res.redirect(`/checkout/success/${order.id}`);
 }

ec25f391da0f1b8c23542cbcbc3ec28ddb7d147e
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-07-27 08:34:20 +0700

    2.2. Đăng ký tài khoản

---
+++ controllers/client/user.controller.js
@@ -0,0 +1,24 @@
+const md5 = require("md5");
+
+const generateHelper = require("../../helpers/generate.helper");
+
+// [GET] /user/register
+module.exports.register = async (req, res) => {
+    res.render("client/pages/user/register", {
+        pageTitle: "Đăng ký tài khoản",
+    });
+}
+
+// [POST] /user/register
+module.exports.registerPost = async (req, res) => {
+    const userData = {
+        fullName: req.body.fullName,
+        email: req.body.email,
+        password: md5(req.body.password),
+        tokenUser: generateHelper.generateRandomString(30)
+    };
+
+    console.log(userData);
+    
+    res.send("Ok");
+}

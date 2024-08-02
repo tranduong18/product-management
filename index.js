cbac0ba45f22da3dfdda3f3a752fb13662a716c8
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-08-02 20:51:03 +0700

    5. Làm trang 404

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin;
 routeAdmin.index(app);
 routeClient.index(app);
 
+app.get("*", (req, res) => {
+    res.render("client/pages/errors/404", {
+        pageTitle: "404 Not Found"
+    });
+});
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });

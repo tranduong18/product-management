bb56ac6f2393eb1914abf135de4649b7511008e5
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-08-23 08:22:23 +0700

    1.1. Tạo collection rooms-chat

---
+++ models/rooms-chat.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const roomChatSchema = new mongoose.Schema({
+  title: String,
+  // avatar: String,
+  typeRoom: String,
+  users: [
+    {
+      userId: String,
+      role: String
+    }
+  ],
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+}, {
+  timestamps: true
+});
+
+const RoomChat = mongoose.model("RoomChat", roomChatSchema, "rooms-chat");
+
+module.exports = RoomChat;
